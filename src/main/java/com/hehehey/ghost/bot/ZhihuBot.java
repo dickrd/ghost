4bf9f822171a588eb1269c7892f069c1aad97622
DickRD <dickdata7@gmail.com>
2017-08-29 11:29:30 +0800

    Add zhihu member api.

--- src/main/java/com/hehehey/ghost/bot/ZhihuBot.java
+++ src/main/java/com/hehehey/ghost/bot/ZhihuBot.java
@@ -17,6 +17,9 @@ import org.bson.Document;
 
 import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,12 +31,15 @@ public class ZhihuBot {
     private static final Logger logger = Logger.getLogger(ZhihuBot.class.getName());
     private static final HttpClient httpClient = new HttpClient("cookie.zhihu.obj");
     private static final Gson gson = new Gson();
-    private static final int answerPerPage = 30;
+    private static final int answerPerPage = 20;
+    private static final long memberUpdateTimeSpan = 7 * 24 * 60 * 60;
 
     private static final String questionApiUrl = "https://www.zhihu.com/api/v4/questions/{0}/answers?sort_by=default" +
             "&include=data%5B*%5D.content%2Cvoteup_count%2Ccreated_time%2Cupdated_time%3Bdata%5B*%5D.author.follower_count" +
             "&limit={1}" +
             "&offset={2}";
+    private static final String memberApiUrl = "https://www.zhihu.com/api/v4/members/{0}?include=" +
+            "locations%2Cemployments%2Cgender%2Ceducations%2Cbusiness%2Cvoteup_count%2Cthanked_count%2Cfollower_count%2Cfollowing_count%2Cavatar_url%2Cdescription";
 
     private final MongoDatabase database;
 
@@ -43,17 +49,75 @@ public class ZhihuBot {
         database = mongoClient.getDatabase("zhihu");
     }
 
-    @SuppressWarnings("SameParameterValue")
-    private void save(String collection, Document filter, Document data) {
-        MongoCollection<Document> zhihu = database.getCollection(collection);
-        zhihu.updateOne(filter, new Document("$set", data), new UpdateOptions().upsert(true));
+    private void getMember(Header authorizationHeader, String memberId) {
+        MongoCollection<Document> zhihu = database.getCollection("member");
+        Member member = new Member();
+
+        boolean passedUpdateDue = true;
+        for (Document found: zhihu.find(new Document("memberId", memberId))) {
+            if (member.timestamp - found.getLong("timestamp") < memberUpdateTimeSpan)
+                passedUpdateDue = false;
+        }
+        if (!passedUpdateDue) {
+            logger.fine("Member already up-to-date: " + memberId);
+            return;
+        }
+        else {
+            logger.fine("Update member: " + memberId);
+        }
+
+        String url = MessageFormat.format(memberApiUrl, memberId);
+        JsonObject jsonObject;
+        try {
+            String responseString = httpClient.getAsString(url,
+                    Charsets.UTF_8.toString(),
+                    new Header[]{authorizationHeader});
+            jsonObject = gson.fromJson(responseString, JsonObject.class);
+        } catch (IOException e) {
+            logger.warning("Network error: " + e);
+            return;
+        } catch (Exception e) {
+            logger.warning("Api failed: " + e);
+            return;
+        }
+
+        member.memberId = jsonObject.get("id").getAsString();
+        member.name = jsonObject.get("name").getAsString();
+        member.gender = jsonObject.get("gender").getAsInt();
+        member.avatarUrl = jsonObject.get("avatar_url").getAsString();
+        member.description = jsonObject.get("description").getAsString();
+        member.headline = jsonObject.get("headline").getAsString();
+
+        member.voteCount = jsonObject.get("voteup_count").getAsInt();
+        member.thanksCount = jsonObject.get("thanked_count").getAsInt();
+        member.followerCount = jsonObject.get("follower_count").getAsInt();
+        member.followingCount = jsonObject.get("following_count").getAsInt();
+
+        for (JsonElement element: jsonObject.get("locations").getAsJsonArray()) {
+            member.location.add(element.getAsJsonObject().get("name").getAsString());
+        }
+        for (JsonElement element: jsonObject.get("educations").getAsJsonArray()) {
+            for (Map.Entry<String, JsonElement> entry: element.getAsJsonObject().entrySet()) {
+                member.education.add(entry.getValue().getAsJsonObject().get("name").getAsString());
+            }
+        }
+        for (JsonElement element: jsonObject.get("employments").getAsJsonArray()) {
+            for (Map.Entry<String, JsonElement> entry: element.getAsJsonObject().entrySet()) {
+                member.employment.add(entry.getValue().getAsJsonObject().get("name").getAsString());
+            }
+        }
+        for (JsonElement element: jsonObject.get("business").getAsJsonArray()) {
+            member.business.add(element.getAsJsonObject().get("name").getAsString());
+        }
+
+        zhihu.insertOne(Document.parse(gson.toJson(member)));
     }
 
     private void getAnswer(Header authorizationHeader, String questionId) {
         String url = MessageFormat.format(questionApiUrl, questionId, answerPerPage, 0);
         boolean isEnd;
         while (true) {
-            logger.info("Parsing: " + url);
+            logger.fine("Parsing: " + url);
             JsonObject jsonObject;
             try {
                 String responseString = httpClient.getAsString(url,
@@ -90,9 +154,11 @@ public class ZhihuBot {
                 JsonObject questionObject = currentObject.getAsJsonObject("question");
                 answer.questionId = questionObject.get("id").getAsLong();
 
-                save("answer",
-                        new Document().append("answerId", answer.answerId).append("updatedAt", answer.updatedAt),
-                        Document.parse(gson.toJson(answer)));
+                MongoCollection<Document> zhihu = database.getCollection("answer");
+                zhihu.updateOne(new Document().append("answerId", answer.answerId).append("updatedAt", answer.updatedAt),
+                        new Document("$set", Document.parse(gson.toJson(answer))),
+                        new UpdateOptions().upsert(true));
+                getMember(authorizationHeader, answer.authorId);
             }
 
             if (isEnd) {
@@ -177,6 +243,35 @@ public class ZhihuBot {
 
         Answer() {
             timestamp = System.currentTimeMillis() / 1000;
+        }
+    }
+
+    public static class Member {
+        long timestamp;
+        String memberId;
+        String name;
+        int gender;
+        String avatarUrl;
+        String description;
+        String headline;
+
+        int voteCount;
+        int thanksCount;
+        int followerCount;
+        int followingCount;
+
+        List<String> location;
+        List<String> education;
+        List<String> employment;
+        List<String> business;
+
+        Member() {
+            timestamp = System.currentTimeMillis() / 1000;
+
+            location = new ArrayList<>();
+            education = new ArrayList<>();
+            employment = new ArrayList<>();
+            business = new ArrayList<>();
         }
     }
 }
